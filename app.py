39f47ba2a268e1f31cc61b2d02359f7a4497961e
Sa1m <71994818+Sa1m@users.noreply.github.com>
2020-10-09 23:31:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask
-from flask import request
-from flask.json import jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello, Flask!"
 
 @app.route('/json-example', methods=['POST']) #GET requests will be blocked
 def json_example():
     req_data = request.get_json()
     print (req_data)
     return jsonify (req_data)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
